617c815e7e6f7a0081d06619ce8b26f2d3985732
Ceagah2 <carlosceagah@gmail.com>
2023-05-20 18:40:12 -0300

    chore: add new class. API

---
+++ Aulas/Aula 06 - Apis/index.js
@@ -0,0 +1,35 @@
+// O que é uma Api ?
+`
+  Application Programming Interfaces
+  Uma API é como um mensageiro que permite que diferentes aplicativos de software se comuniquem entre si.
+  Use uma analogia do mundo real, como fazer um pedido de comida em um restaurante através de um garçom. 
+  O garçom (API) anota o seu pedido (requisição), se comunica com a cozinha (servidor) e traz a comida (resposta).
+`
+// Definições de API
+`
+um conjunto de regras e protocolos que governam como os componentes de software devem interagir.
+APIs permitem que os desenvolvedores utilizem funcionalidades pré-construídas de outros sistemas 
+de software sem precisar entender seu funcionamento interno.
+`
+// Tipos de API
+`
+REST, SOAP: 
+SDK:
+`
+// Componentes da API
+`
+Requisição: As informações enviadas à API para realizar uma ação específica ou recuperar dados.
+Resposta: Os dados retornados pela API em resposta à requisição.
+`
+// Workflow da API
+`
+O cliente (aplicativo do desenvolvedor) envia uma requisição para o servidor (API) com parâmetros específicos.
+O servidor processa a requisição e realiza as ações necessárias.
+O servidor envia de volta uma resposta contendo os dados solicitados ou informações sobre a ação realizada.
+`
+// Documentação
+`
+Documentação fornece informações sobre os endpoints disponíveis, parâmetros de requisição, 
+formatos de resposta e exemplos de como usar a API.
+`
+// Exemplo prático
